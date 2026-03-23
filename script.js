03cd7600ed9822ea47a037191153efd24f6c59cd
lucasbsilva84-del <lucas.b.silva84@aluno.senai.br>
2026-03-23 11:19:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+// function calcularImc(peso, altura) {
+//   return peso / Math.pow(altura, 2);
+// }
+
+// function classificarImc(imc) {
+//   if (imc < 18.5) {
+//     return "Abaixo do peso";
+//   } else if (imc >= 18.5 && imc < 25) {
+//     return "Peso normal";
+//   } else if (imc >= 25 && imc < 30) {
+//     return "Sobrepeso";
+//   } else if (imc >= 35 && imc < 40) {
+//     return "Obeso";
+//   } else {
+//     return "Obesidade grau III";
+//   }
+// }
+
+
+// let imc = calcularImc(110, 1.76);
+// let classificacao = classificarImc(imc);
+
+// console.log("IMC:", imc.toFixed(2));
+// console.log("Classificação:", classificacao);
+
+
+
+//////////////////////////////1////////////////////////////
+// function saudar() {
+//     console.log('Ola seja bem vindo ')
+// }
+// saudar()
+//////////////////////////////2////////////////////////////,
+// function mostrarNome(nome) {
+//   console.log(`Olá, ${nome}`);
+// }
+
+
+// mostrarNome("lucas");
+//////////////////////////////3////////////////////////////
+
+// function  soma (num1 , num2) {
+//     return num1+num2;
+// }
+// let resultado = (soma (90 , 20))
+// console.log (`sua soma é ${resultado}`)
+//////////////////////////////4////////////////////////////
+// function  dobro (num1) {
+//     return num1*2;
+// }
+// let resultado = (dobro (20 ))
+// console.log (`sua soma é ${resultado}`)
+//////////////////////////////5////////////////////////////
+// function  parOuImpar(numero) {
+//     if (numero % 2 === 0) {
+//         console.log(`seu numero é par`);
+
+//     }
+//     else{
+//         console.log(`seu numero é ímpar`)
+//     }
+// }
+// parOuImpar (1)
+//////////////////////////////6////////////////////////////
+// function maiorNumero(num, num2) {
+//     if (num > num2) {
+//         console.log(`O número ${num} é maior que o ${num2}`);
+//     } else {
+//         console.log(`O número ${num2} é maior que o ${num}`);
+//     }
+// }
+
+// maiorNumero(10, 20);
+//////////////////////////////7////////////////////////////
+
+// function media(n1, n2, n3) {
+//     return (n1 + n2 + n3) / 3;
+// }
+// let resultado = media(9 , 3 , 1)
+// console.log (resultado.toFixed(2))
+
+//////////////////////////////8////////////////////////////
+//  function converterCelsius() {
+// let n1 = Number(prompt("Ensira o número"));
+
+// let conversao = (n1 * 9/5) + 32
+// console.log("A conversão fica: " + conversao)
+// }
+
+// converterCelsius()
+
+//////////////////////////////9////////////////////////////
+// function contador() {
+//     let n1 = Number(prompt("Insira o número")); 
+
+//     for (let i = 1; i <= n1; i++) {
+//         console.log(i);
+//     }
+// }
+
+// contador();
+//////////////////////////////10////////////////////////////
+// function contador() {
+//     let n1 = Number(prompt("Insira o número")); 
+
+//     for (let i = 1; i <= n1; i++) {
+//         console.log(i);
+//     }
+// }
+
+// contador();
